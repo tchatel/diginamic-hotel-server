bfe4da64f95f491dd41be316d437dd7259f3466f
Thierry Chatel <tchatel@gmail.com>
2020-06-24 17:31:49 +0200

    periods

--- src/periods/periods.controller.ts
+++ src/periods/periods.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Put, HttpCode, HttpStatus, Body, Post, Delete, Query, UsePipes, ValidationPipe, HttpException } from '@nestjs/common';
+import { Period } from './period.entity';
+import { PeriodsService } from './periods.service';
+import { PeriodDto } from './period.dto';
+import { ApiQuery } from '@nestjs/swagger';
 
-@Controller('periods')
-export class PeriodsController {}
+@Controller('admin/periods')
+@UsePipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true, transform: true}))
+export class PeriodsController {
+
+    constructor(private periodsSrv: PeriodsService) {
+    }
+
+    @Get()          //   /admin/periods?category=3&start=2020-06-24&end=2020-07-02
+    @ApiQuery({name: 'category', required: false})
+    @ApiQuery({name: 'start', required: false})
+    @ApiQuery({name: 'end', required: false})
+    searchAll(@Query('category') categoryId?: number,
+              @Query('start') startDate?: string,
+              @Query('end') endDate?: string): Promise<Period[]> {
+        return this.periodsSrv.searchAll({categoryId, startDate, endDate});
+    }
+
+    @Get(':id')
+    readOne(@Param('id', ParseIntPipe) id: number): Promise<Period> {
+        return this.periodsSrv.readOne(id);
+    }
+
+    @Post()
+    create(@Body() periodDto: PeriodDto): Promise<Period> {
+        if (periodDto.startDate > periodDto.endDate) {
+            throw new HttpException('end date must be after start date', HttpStatus.BAD_REQUEST);
+        }
+        return this.periodsSrv.create(periodDto);
+    }
+
+    @Put(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    update(@Param('id', ParseIntPipe) id: number,
+           @Body() periodDto: PeriodDto): Promise<void> {
+        if (periodDto.startDate > periodDto.endDate) {
+            throw new HttpException('end date must be after start date', HttpStatus.BAD_REQUEST);
+        }
+        return this.periodsSrv.update(id, periodDto);
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.periodsSrv.delete(id);
+    }
+
+}
--- src/periods/periods.module.ts
+++ src/periods/periods.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PeriodsController } from './periods.controller';
 import { PeriodsService } from './periods.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Period } from './period.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Period])
+  ],
   controllers: [PeriodsController],
   providers: [PeriodsService]
 })
